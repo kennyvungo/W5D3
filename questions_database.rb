2b01a5056de8156b69d7a5299b82cb40f830255a
kennyvngo <kennyvungo@gmail.com>
2023-04-26 14:30:31 -0700

    finished initialize and attr accessors

--- questions_database.rb
+++ questions_database.rb
@@ -11,11 +11,30 @@ class QuestionsDatabase < SQLite3::Database
     end
 end
 
-class User
 
+
+
+class User
+    attr_accessor :id,:fname,:lname
+    def self.find_by_id(id)
+        query = QuestionsDatabase.instance.execute(<<-SQL, id)
+                SELECT * FROM users
+                WHERE id = ?
+            SQL
+
+        return nil unless query.length > 0
+        User.new(query.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
+    end
 end
 
 class Question
+    attr_accessor :id,:title,:body,:author_id
     def self.find_by_id(id)
         query = QuestionsDatabase.instance.execute(<<-SQL, id)
                 SELECT * FROM questions
@@ -26,15 +45,71 @@ class Question
         Question.new(query.first)
     end
 
-    def initialize
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end
 
 end
 
 class QuestionFollow
+    attr_accessor :id,:user_id,:question_id
+        def self.find_by_id(id)
+        query = QuestionsDatabase.instance.execute(<<-SQL, id)
+                SELECT * FROM question_follows
+                WHERE id = ?
+            SQL
+
+        return nil unless query.length > 0
+        QuestionFollow.new(query.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+    end
 end
 
 class Reply
+    :id,:question_id,:parent_reply_id,:author_id,:body
+        def self.find_by_id(id)
+        query = QuestionsDatabase.instance.execute(<<-SQL, id)
+                SELECT * FROM replies
+                WHERE id = ?
+            SQL
+
+        return nil unless query.length > 0
+        Reply.new(query.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @parent_reply_id = options['parent_reply_id']
+        @author_id = options['author_id']
+        @body = options['body']
+        
+    end
 end
 
 class Like
+    attr_accessor :id,:user_id,:question_id
+        def self.find_by_id(id)
+        query = QuestionsDatabase.instance.execute(<<-SQL, id)
+                SELECT * FROM question_likes
+                WHERE id = ?
+            SQL
+
+        return nil unless query.length > 0
+        Like.new(query.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+    end
 end
